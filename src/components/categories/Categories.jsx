16e64eac9c49520caf6c3a363a42dd37eb485b20
Pritish-Sinha <bpb1789@gmail.com>
2023-11-25 20:44:39 +0530

    changes

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -6,9 +6,7 @@ const categoriesData = [
   { title: 'Dentist', description: 'Teething troubles? Schedule a dental checkup', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-dentist@2x.jpg' },
   { title: 'Gynecologist/Obstetrician', description: 'Explore women\'s health, pregnancy, and infertility treatments', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-gynecologist@2x.jpg' },
   { title: 'Dietitian/Nutrition', description: 'Get guidance on eating right, weight management, and sports nutrition', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-dietitian@2x.jpg' },
-  { title: 'Physiotherapist', description: 'Pulled a muscle? Get it treated by a trained physiotherapist', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-physiotherapist@2x.jpg' },
-  { title: 'General surgeon', description: 'Need to get operated? Find the right surgeon', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-general-surgeon@2x.jpg' },
-  { title: 'Orthopedist', description: 'For Bone and Joints issues, spinal injuries, and more', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-orthopedist@2x.jpg' },
+  { title: 'Physiotherapist', description: 'Pulled a muscle? Get it treated by a trained physiotherapist', image: 'https://www.practostatic.com/consumer-home/desktop/images/1558283618/sp-physiotherapist@2x.jpg' }
 ];
 
 const Categories = ({ setCategories }) => {
